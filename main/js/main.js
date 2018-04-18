05b603854b213b1a989ffdcc5d356eb49288f310
Cyril Napolitano <38205245+Sadejoro@users.noreply.github.com>
2018-04-18 10:03:48 +0200

    Add files via upload

--- main/js/main.js
+++ main/js/main.js
@@ -3,52 +3,28 @@ $(function() {
      * Smooth scrolling
      **/
     $("a[href*='#']:not([href='#'])").click(function() {
-        if (
-            location.hostname == this.hostname &&
-            this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
-        ) {
+        if (location.hostname == this.hostname && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")) {
             var anchor = $(this.hash);
             anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
             if (anchor.length) {
-                $("html, body").animate({ scrollTop: anchor.offset().top + 0 }, 1500);
+                $("html, body").animate({
+                    scrollTop: anchor.offset().top + 0
+                }, 1500);
             }
         }
     });
-
-
-
-
+    //$Change l'état de la barre de navigation au scroll
     function navScroll() {
-
-        
         $(window).scroll(function() {
             scrollTop = $(window).scrollTop();
-
-
             if (scrollTop >= 30) {
                 $('.header-nav').addClass('header-nav_scrolled');
             } else if (scrollTop < 30) {
                 $('.header-nav').removeClass('header-nav_scrolled');
             }
-
-
-            // Reset beforeScroll to afterScroll position
-
-
         });
     }
     navScroll();
 
-
-    var text = $(".text");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= 200) {
-            text.removeClass("hidden");
-        } else {
-            text.addClass("hidden");
-        }
-    });
-
+    
 });
